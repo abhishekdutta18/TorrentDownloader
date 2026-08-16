950622dd6bb2f2219833c9809fb298bc417da657
abhishekdutta18 <abhishek.dutta1996@gmail.com>
2026-08-17 00:52:10 +0530

    chore: remove test script

--- test-ipc.js
+++
@@ -1,2 +0,0 @@
-const { ipcRenderer } = require('electron')
-ipcRenderer.invoke('add-torrent', 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel').then(console.log).catch(console.error)
