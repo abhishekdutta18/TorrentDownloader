89322d38a70abab7b81c0ff03a472a6603d30761
abhishekdutta18 <abhishek.dutta1996@gmail.com>
2026-08-30 19:33:20 +0530

    chore: remove obsolete Electron, Capacitor, and scratch files

--- android/app/src/main/java/com/torrentdownloader/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.torrentdownloader.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- worker.js
+++
@@ -1,5 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    return new Response("Torrent Downloader Desktop App - Cloudflare Worker Placeholder", { status: 200 });
-  }
-}
